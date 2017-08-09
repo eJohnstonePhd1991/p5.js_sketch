120574b521bc478e1ef5ab31e7f762bd8928c141
Ewan Johnstone <30697825+eJohnstonePhd1991@users.noreply.github.com>
2017-08-09 20:04:16 +0100

    Wrote a basic fractal

--- shifman_p5/sketch.js
+++ shifman_p5/sketch.js
@@ -1,17 +1,51 @@
+var slider;
 function setup() {
-    createCanvas(600,400);
-   
+    createCanvas(600,600);
+    //this slider controls the angle of the branches
+    slider = createSlider(0, PI/2, PI/4,0.01);
     
 
 }
 
 function draw() {
-    background(0);
-    stroke(255,0,0);
-    ellipse(100,100,50,50);
-    
+    background(50);
+    stroke(255);
+    //sets coordinate origin to the bottom middle of the screen
+    translate(300,height);
+    angle = slider.value();
+    branch(150);
+}
+
+function branch(len) {
+    //draws the stem
+    line(0, 0,0,-len);
+    // moves the origin to the top of the stem
+    translate(0,-len);
+    if (len >10){
+        //draw left branch
+        push();
+        rotate(-angle);
+        branch(len*0.67);
+        pop();
+        //draw middle branch
+        push();
+        branch(len*0.67)
+        pop();
+      
+        //draw right branch
+        push();
+        rotate(angle);
+        branch(len*0.67);
+        pop();
+        
+    }
+   
+        
+    }
     
    
     
-  
-}
+    
+    
+    
+    
