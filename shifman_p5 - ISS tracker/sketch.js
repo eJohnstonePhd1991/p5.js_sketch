a9a0985b78916cc3a48db90574b62b6a5149ca56
Ewan Johnstone <30697825+eJohnstonePhd1991@users.noreply.github.com>
2017-10-20 15:19:11 +0100

    Webpage which displays ISS gps data

---
+++ shifman_p5 - ISS tracker/sketch.js
@@ -0,0 +1,110 @@
+
+
+var mapimg;
+
+var clat = 0;
+var clon = 0;
+var issX;
+var issY;
+var cx;
+var cy;
+var ww = 1024;
+var hh = 512;
+
+var zoom = 1;
+var coordBox
+
+function preload() {
+    var token = '?access_token=pk.eyJ1IjoiZWpvaG5zdG9uZTE5OTEiLCJhIjoiY2o3MHJwY2c2MDhuaDM1cXI4MjNhcXQ4NSJ9.OV6oOSiqSSCqRFn_N2kVNw';
+ 
+  mapimg = loadImage('https://api.mapbox.com/styles/v1/mapbox/dark-v9/static/' +
+    clon + ',' + clat + ',' + zoom + '/' +
+    ww + 'x' + hh +
+    token);
+ 
+}
+
+
+
+
+function setup() {
+  createCanvas(ww, hh);
+
+  setInterval(askIss,1000);
+
+  cx = mercX(clon);
+  cy = mercY(clat);
+    
+    coordBox = createDiv("Latitude:");
+    
+     
+  
+    
+    
+}
+
+function draw(){
+    translate(width / 2, height / 2);
+    imageMode(CENTER);
+    image(mapimg, 0, 0);
+
+    
+    var x = mercX(0) - cx;
+    var y = mercY(0) - cy;
+    
+      
+     if(x < - width/2) {
+      x += width;
+    } else if(x > width / 2) {
+      x -= width;
+    }
+    
+    
+    stroke(255, 0, 255);
+    fill(255, 0, 255, 200);
+    ellipse(x, y, 10, 10);
+    ellipse(issX, issY, 10, 10);
+    
+    
+}
+
+function askIss() {
+    loadJSON('http://api.open-notify.org/iss-now.json',getData, 'jsonp');
+
+}
+function getData(data) {
+  var issLat = Number(data.iss_position.latitude);
+    var issLong = Number(data.iss_position.longitude);   
+    
+    coordBox.html("Lat:" + issLat + " Long:" + issLong);
+    
+    
+    
+ 
+    issX = mercX(issLong) - cx;
+    issY = mercY(issLat) - cy;
+    
+    
+     if(issX < - width/2) {
+       issX += width;
+     } else if(issX > width / 2) {
+       issX -= width;
+     }
+       
+}
+
+
+function mercX(lon) {
+  lon = radians(lon);
+  var a = (256 / PI) * pow(2, zoom);
+  var b = lon + PI;
+  return a * b;
+}
+
+function mercY(lat) {
+  lat = radians(lat);
+  var a = (256 / PI) * pow(2, zoom);
+  var b = tan(PI / 4 + lat / 2);
+  var c = PI - log(b);
+  return a * c;
+}
